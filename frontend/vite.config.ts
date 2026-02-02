efdf3322e1c02b78215171acff029edcd26e8241
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-02-02 10:52:53 +0800

    deployment fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/VON.CORE_monorepo/",
+    base: "/",
 
     server: {
       port: 3000,
